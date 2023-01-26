9ac0d7fd60942b39f5aec867259d9948a7dfa10d
Own Kingdom <113741344+Guru098@users.noreply.github.com>
2023-01-26 09:43:34 +0530

    Update song.py

--- AlexaMusic/utils/inline/song.py
+++ AlexaMusic/utils/inline/song.py
@@ -27,7 +27,7 @@ def song_markup(_, vidid):
         [
             InlineKeyboardButton(
                 text="• sᴜᴩᴩᴏʀᴛ •",
-                url="https://t.me/DevilsHeavenMF",
+                url="https://t.me/botsuppot",
             ),
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ],
